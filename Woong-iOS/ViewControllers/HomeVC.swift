cf9a0c6a209e22003972eb5a0912dc776d2e6ce5
Leeseungsoo <few280@gmail.com>
2018-07-01 15:16:38 +0900

    #4 Add main tab sub VCs

---
+++ Woong-iOS/ViewControllers/HomeVC.swift
@@ -0,0 +1,27 @@
+//
+//  HomeVC.swift
+//  Woong-iOS
+//
+//  Created by Leeseungsoo on 2018. 7. 1..
+//  Copyright © 2018년 Leess. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
