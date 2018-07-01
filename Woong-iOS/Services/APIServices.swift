e22f7f25d693fd2fe0657dbca8690473c425c447
Leeseungsoo <few280@gmail.com>
2018-07-01 14:01:49 +0900

    #0 make project woong-ios

---
+++ Woong-iOS/Services/APIServices.swift
@@ -0,0 +1,16 @@
+//
+//  APIServices.swift
+//  Woong-iOS
+//
+//  Created by Leeseungsoo on 2018. 7. 1..
+//  Copyright © 2018년 Leess. All rights reserved.
+//
+
+protocol APIService {
+}
+
+extension APIService {
+    static func url(_ path: String) -> String {
+        return "" + path
+    }
+}
---
+++ Woong-iOS/ViewControllers/SplashVC.swift
@@ -0,0 +1,27 @@
+//
+//  SplashVC.swift
+//  Woong-iOS
+//
+//  Created by Leeseungsoo on 2018. 7. 1..
+//  Copyright © 2018년 Leess. All rights reserved.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
