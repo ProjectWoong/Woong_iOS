ae951a9fdf2da1bcd8723e0e6c2d41a787a40ce6
Leeseungsoo <few280@gmail.com>
2018-07-06 15:31:33 +0900

    #커밋메세지 넘버링 방식 변경 -> ( #[git id] - [user's commit number] - MM/dd hh/mm) #few28 - 1 - 07/06 15:31

--- Woong-iOS/ViewControllers/SellerVC.swift
+++ Woong-iOS/ViewControllers/SellerVC.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class SellerVC: UIViewController {
     
-    let sellerCellId = "SellerMenuCell"
+    let menuCellId = "SellerMenuCell"
+    let infoCellId = "SellerInfoCell"
     let menu = ["소개", "물품", "앨범", "후기"]
+    
     var selectedIndexPath = IndexPath(item: 0, section: 0)
     
     var sellerMenuConstraint: NSLayoutConstraint?
@@ -23,6 +25,9 @@ class SellerVC: UIViewController {
     @IBOutlet var sellerProfileView: UIView!
     @IBOutlet var sellerMenuCollectionView: UICollectionView!
     @IBOutlet var sellerMenuView: UIView!
+    
+    @IBOutlet var sellerInfoCollectionView: UICollectionView!
+    
     var horizontalBarLeftAnchorConstraint: NSLayoutConstraint?
     let horizontalBarView: UIView = {
         let view = UIView()
@@ -45,8 +50,10 @@ class SellerVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     func testAction(_ sender: Any) {
-        for i in 0 ..< 4 {
+        for i in 0 ..< menu.count {
             let cell = sellerMenuCollectionView.cellForItem(at: IndexPath(item: i, section: 0)) as! SellerMenuCell
             cell.setSelectedColor(UIColor.black)
             sellerMenuCollectionView.selectItem(at: selectedIndexPath, animated: true, scrollPosition: .init(rawValue: 0))
@@ -58,6 +65,8 @@ class SellerVC: UIViewController {
         let leftBarBtn = UIBarButtonItem(title: "mail", style: .plain, target: self, action: nil)
         self.navigationItem.rightBarButtonItem = rightBarBtn
         self.navigationItem.leftBarButtonItem = leftBarBtn
+        
+        
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
@@ -70,6 +79,8 @@ class SellerVC: UIViewController {
         sellerMenuCollectionView.dataSource = self
         sellerMenuCollectionView.selectItem(at: selectedIndexPath as IndexPath, animated: false, scrollPosition: .init(rawValue: 0))
         
+        sellerInfoCollectionView.delegate = self
+        sellerInfoCollectionView.dataSource = self
     }
     
     private func setupHorizontalBar() {
@@ -86,30 +97,73 @@ class SellerVC: UIViewController {
 
 extension SellerVC: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let y = scrollView.contentOffset.y
-        print(y)
-        if y >= 184 {
-            sellerMenuTopConstraint.constant = y + 64
-        } else if y < 184 {
-            sellerMenuTopConstraint.constant = 248
+        if scrollView == self.scrollView {
+            let scrollY = scrollView.contentOffset.y
+            let collectionY = sellerInfoCollectionView.contentOffset.y
+            
+            if scrollY == 0 {
+                print("1")
+                if collectionY != 0 {
+                    sellerInfoCollectionView.isScrollEnabled = true
+                    print("2-1")
+                } else {
+                    print("2-2")
+                }
+            } else if scrollY < 184 {
+                sellerMenuTopConstraint.constant = 248
+                sellerInfoCollectionView.isScrollEnabled = false
+                self.scrollView.bounces = true
+                print("3")
+            } else if scrollY >= 184 {
+                sellerMenuTopConstraint.constant = scrollY + 64
+                sellerInfoCollectionView.isScrollEnabled = true
+                self.scrollView.bounces = false
+                print("4")
+            }
+            
+        }
+        else {
+            let y = sellerInfoCollectionView.contentOffset.y
+            
+            if y > 1 {
+                sellerInfoCollectionView.bounces = true
+            } else {
+                sellerInfoCollectionView.bounces = false
+            }
         }
     }
+
+    
 }
 
 extension SellerVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == sellerMenuCollectionView {
             return CGSize(width: sellerMenuView.frame.width / 4, height: sellerMenuView.frame.height)
+        } else {
+            return CGSize(width: self.view.frame.width, height: self.view.frame.width)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return menu.count
+        if collectionView == sellerMenuCollectionView {
+            return menu.count
+        } else {
+            return 5
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = sellerMenuCollectionView.dequeueReusableCell(withReuseIdentifier: sellerCellId, for: indexPath) as! SellerMenuCell
-        cell.tintColor = UIColor.rgb(red: 173, green: 173, blue: 173)
-        cell.menuLabel.text = menu[indexPath.item]
-        return cell
+        if collectionView == sellerMenuCollectionView {
+            let cell = sellerMenuCollectionView.dequeueReusableCell(withReuseIdentifier: menuCellId, for: indexPath) as! SellerMenuCell
+            cell.tintColor = UIColor.rgb(red: 173, green: 173, blue: 173)
+            cell.menuLabel.text = menu[indexPath.item]
+            return cell
+        } else {
+            let cell = sellerInfoCollectionView.dequeueReusableCell(withReuseIdentifier: infoCellId, for: indexPath) as! SellerInfoCell
+            return cell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
---
+++ Woong-iOS/Views/SellerInfoCell.swift
@@ -0,0 +1,16 @@
+//
+//  SellerInfoCell.swift
+//  Woong-iOS
+//
+//  Created by Leeseungsoo on 2018. 7. 5..
+//  Copyright © 2018년 Leess. All rights reserved.
+//
+
+import UIKit
+
+class SellerInfoCell: UICollectionViewCell {
+    
+    
+    
+    
+}
