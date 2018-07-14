134820b54abbb0757a6b99cbb21cb598e7afc52a
pkse1213 <pkse1213@naver.com>
2018-07-14 11:24:13 +0900

    #pkse1213 - 12

---
+++ Woong-iOS/Views/CartSumPriceCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartSumPriceCell.swift
+//  Woong-iOS
+//
+//  Created by 박세은 on 2018. 7. 14..
+//  Copyright © 2018년 Leess. All rights reserved.
+//
+
+import UIKit
+
+class CartSumPriceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
