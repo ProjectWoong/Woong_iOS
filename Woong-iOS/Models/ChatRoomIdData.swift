3e69971e10218194bfd75b19b1250193b602b252
pkse1213 <pkse1213@naver.com>
2018-07-26 00:42:21 +0900

    나머지 수정

---
+++ Woong-iOS/Models/ChatRoomIdData.swift
@@ -0,0 +1,9 @@
+//
+//  ChatRoomIdData.swift
+//  Woong-iOS
+//
+//  Created by 박세은 on 2018. 7. 19..
+//  Copyright © 2018년 Leess. All rights reserved.
+//
+
+import Foundation
---
+++ Woong-iOS/Services/ProductFavoriteService.swift
@@ -0,0 +1,9 @@
+//
+//  ProductFavoriteService.swift
+//  Woong-iOS
+//
+//  Created by 박세은 on 2018. 7. 20..
+//  Copyright © 2018년 Leess. All rights reserved.
+//
+
+import Foundation
